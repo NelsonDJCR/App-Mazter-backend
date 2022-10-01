5de480bd1eed6e0a80d7f10379fec9bd1fdf63f8
NelsonDJCR <dev@nelsondjcr.com>
2022-10-01 02:06:40 -0500

    Add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-use \App\Models\BusinessType;
 
+use \App\Models\BusinessType;
+use \App\Models\Product;
+use App\Models\Store;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,13 +17,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         BusinessType::create(['name' => 'Supermercado']);
         BusinessType::create(['name' => 'Tecnología']);
+
+        Store::create([
+            'comercial_name' => 'Store 1',
+            'propetiary_name' => 'Propetiary 1',
+            'email' => 'st1',
+            'phone' => '312312312',
+            'password' => bcrypt('123'),
+            'business_type_id' => 1,
+        ]);
+
+        Product::create(['name' => 'Soup','price' => '1231','store_id' => 1]);
+        Product::create(['name' => 'Chess','price' => '5221','store_id' => 1]);
+        Product::create(['name' => 'Bread','price' => '15000','store_id' => 1]);
+
+        Store::create([
+            'comercial_name' => 'Store 2',
+            'propetiary_name' => 'Propetiary 2',
+            'email' => 'st2',
+            'phone' => '312312312',
+            'password' => bcrypt('123'),
+            'business_type_id' => 1,
+        ]);
+
+        Product::create(['name' => 'USB','price' => '1323','store_id' => 2]);
+        Product::create(['name' => 'Airpods','price' => '123','store_id' => 2]);
+        Product::create(['name' => 'Phone','price' => '1231231','store_id' => 2]);
+
+
     }
 }
