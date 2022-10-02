c3088b2c08a287ef68f2de96720d550f1d5bbd61
NelsonDJCR <dev@nelsondjcr.com>
2022-10-01 22:14:37 -0500

    Send backup for email

--- app/Console/Commands/SendBackup.php
+++ app/Console/Commands/SendBackup.php
@@ -2,8 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Mail\SendBackup as MailSendBackup;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
 
 class SendBackup extends Command
 {
@@ -14,30 +17,52 @@ class SendBackup extends Command
 
     public function handle()
     {
+        echo "_________________________\n";
+        echo "Realizando Backup...\n";
         $id = $this->argument('id');
-        
-        $products = Product::where('user_id',$id)->get();
+
+        $products = Product::where('user_id', $id)->get();
 
         $sql = "";
 
+        $user = User::find($id);
+
+        #Get SQL for update data user
+        $sql .= "UPDATE users SET " .
+            " comercial_name = '" . $user->comercial_name .
+            "',propetiary_name = '" . $user->propetiary_name .
+            "',email = '" . $user->email .
+            "',phone = '" . $user->phone .
+            "',phone_secondary = " . ($user->phone_secondary ? $user->phone_secondary : "null") .
+            ",color = '" . $user->color .
+            "',state_suscription = '" . $user->state_suscription .
+            "',business_type_id = '" . $user->business_type_id . "' WHERE id = $id; \n\n\n";
+
+
+        # Get SQL for each product register 
         foreach ($products as $key) {
-            $sql.= "INSERT INTO products (
-                'name','bar_code','price','discount','sales','stock','amount_products','user_id','state','route_image',
-            ) VALUES (".
-                $key->name.",".
-                $key->bar_code.",".
-                $key->price.",".
-                $key->discount.",".
-                $key->sales.",".
-                $key->stock.",".
-                $key->amount_products.",".
-                $key->user_id.",".
-                $key->state.",".$key->route_image.","
-            ."); ";           
+            $sql .= "INSERT INTO products (name,bar_code,price,discount,sales,stock,amount_products,user_id,state,route_image) 
+            VALUES (\"" .
+                $key->name . "\"," .
+                ($key->bar_code ? $key->bar_code : "null")  . "," .
+                $key->price . "," .
+                $key->discount . "," .
+                $key->sales . "," .
+                $key->stock . "," .
+                $key->amount_products . "," .
+                $key->user_id . "," .
+                $key->state . "," .
+                ($key->route_image ? $key->route_image : "null")
+                . "); ";
         }
 
-        dd($sql);
-        
+        # Send Email with SQL text
+        $data = $sql;
+        $mail = new MailSendBackup($data);
+        Mail::to('je.nelsondjcr@gmail.com')->send($mail);
+
+        echo "Backup finalizado\n";
+        echo "_________________________\n";
         return Command::SUCCESS;
     }
 }
