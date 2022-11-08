e87a626b2d0988b2a2690aeea21713606a9b1755
Nelson Jimenez <dev@nelsondjcr.com>
2022-11-08 14:22:03 -0500

    Add command key:generate

--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Artisan;
 
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+Route::get('/keygenerate', function ()
+{
+    Artisan::call('key:generate');
+    return "Cleared!";
+});
