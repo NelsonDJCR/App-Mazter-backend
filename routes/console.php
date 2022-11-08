f18fb016368fa3d31d0edb9af765f03bff51811a
Nelson Jimenez <dev@nelsondjcr.com>
2022-11-08 14:25:33 -0500

    Move artisan call

--- routes/console.php
+++ routes/console.php
@@ -8,8 +8,4 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-Route::get('/keygenerate', function ()
-{
-    Artisan::call('key:generate');
-    return "Cleared!";
-});
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     
+});
+Route::get('/keygenerate', function ()
+{
+    Artisan::call('key:generate');
+    return "Cleared!";
 });
